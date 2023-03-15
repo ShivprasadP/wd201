ceecf1e86eb25aded3369ff5d0afba42b38c1311
Shivprasad <105196930+Softwareholic@users.noreply.github.com>
2023-03-15 18:58:03 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,52 +1,65 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http')
+const fs = require('fs')
 
-const args = process.argv.slice(2);
-const portI = args.indexOf('--port');
-const default_port = 5000;
-const port = parseInt(args[portI + 1], 10) || default_port;
+const args = process.argv.slice(2)
+const portI = args.indexOf('--port')
+// const default_port = 5000
+const port = parseInt(args[portI + 1], 10)
 
-let homeContent;
-let projectContent;
-let registrationContent;
+let homeContent
+let projectContent
+let registrationContent
 
-fs.readFile("home.html", (err, home) => {
+fs.readFile('home.html', (err, home) => {
   if (err) {
-    throw err;
+    throw err
   }
-  homeContent = home;
-});
+  homeContent = home
+})
 
-fs.readFile("project.html", (err, project) => {
+fs.readFile('project.html', (err, project) => {
   if (err) {
-    throw err;
+    throw err
   }
-  projectContent = project;
-});
+  projectContent = project
+})
 
-fs.readFile("registration/registration.html", (err, registration) => {
+fs.readFile('registration.html', (err, registration) => {
   if (err) {
-    throw err;
+    throw err
   }
-  registrationContent = registration;
-});
+  registrationContent = registration
+})
 
-http.createServer((request, response) => {
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    let url = request.url;
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-        case "/registration":
-        response.write(registrationContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
-    }
-  })
-  .listen(port);
+const server = http.createServer((request, response) => {
+  if (request.url === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.end(homeContent)
+  } else if (request.url === '/project') {
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.end(projectContent)
+  } else if (request.url === '/registration') {
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.end(registrationContent)
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' })
+    response.end('<h1>404 Not Found</h1>')
+  }
+  // response.writeHeader(200, { 'Content-Type': 'text/html' })
+  // const url = request.url
+  // switch (url) {
+  //   case '/project':
+  //     response.write(projectContent)
+  //     response.end()
+  //     break
+  //   case '/registration':
+  //     response.write(registrationContent)
+  //     response.end()
+  //     break
+  //   default:
+  //     response.write(homeContent)
+  //     response.end()
+  //     break
+  // }
+})
+server.listen(port)
